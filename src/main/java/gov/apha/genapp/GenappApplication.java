543015cd121f86213929a901e48c33748215b064
Dev Kornish <devkornish@gmail.com>
2024-07-31 14:33:31 +0100

    GenAPP starter spring boot application

---
+++ src/main/java/gov/apha/genapp/GenappApplication.java
@@ -0,0 +1,13 @@
+package gov.apha.genapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenappApplication.class, args);
+	}
+
+}
