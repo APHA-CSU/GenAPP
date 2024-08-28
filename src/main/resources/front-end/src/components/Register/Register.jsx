253319a24286082e275065aec6262b2cae67f3b0
Sainab Omar <sainabomar07@gmail.com>
2024-08-28 13:57:40 +0100

    creates & enables form submit button for valid registration inputs

--- src/main/resources/front-end/src/components/Register/Register.jsx
+++ src/main/resources/front-end/src/components/Register/Register.jsx
@@ -63,6 +63,7 @@ const Register = () => {
       setErrMsg("Invalid Entry");
       return;
     }
+    setSuccess(true);
   };
 
   return (
@@ -77,107 +78,131 @@ const Register = () => {
           </h2>
         </Grid>
         <Grid item xs={8} md={4} className="register-grid">
-          <section>
-            <p ref={errRef} className={errMsg ? "errmsg" : "offscreen"}>
-              {errMsg}
-            </p>
-            <h1>Register</h1>
-            <form onSubmit={handleSubmit}>
-              <label htmlFor="email">
-                Email:
-                <FontAwesomeIcon
-                  icon={faCheck}
-                  className={validEmail ? "valid" : "hide"}
-                />
-                <FontAwesomeIcon
-                  icon={faTimes}
-                  className={validEmail || !email ? "hide" : "invalid"}
-                />
-              </label>
-              <input
-                type="text"
-                id="email"
-                ref={emailRef}
-                onChange={(e) => setEmail(e.target.value)}
-                value={email}
-                required
-                onFocus={() => setEmailFocus(true)}
-                onBlur={() => setEmailFocus(false)}
-              />
-              <p
-                id="emailnote"
-                className={
-                  emailFocus && email && !validEmail
-                    ? "instructions"
-                    : "offscreen"
-                }
-              >
-                <FontAwesomeIcon icon={faInfoCircle} />
-                Please enter a valid organisation email.
+          {success ? (
+            <section>
+              <h1>Successfully Registered</h1>
+              <p>
+                <a href="#">Sign In</a>
               </p>
-              <label htmlFor="password">
-                Password:
-                <FontAwesomeIcon
-                  icon={faCheck}
-                  className={validPwd ? "valid" : "hide"}
-                />
-                <FontAwesomeIcon
-                  icon={faTimes}
-                  className={validPwd || !pwd ? "hide" : "invalid"}
-                />
-              </label>
-              <input
-                type="password"
-                id="password"
-                onChange={(e) => setPwd(e.target.value)}
-                value={pwd}
-                required
-                onFocus={() => setPwdFocus(true)}
-                onBlur={() => setPwdFocus(false)}
-              />
-              <p
-                id="pwdnote"
-                className={pwdFocus && !validPwd ? "instructions" : "offscreen"}
-              >
-                <FontAwesomeIcon icon={faInfoCircle} />
-                8 to 24 characters.
-                <br />
-                Must include uppercase and lowercase letters, a number and a
-                special character.
-                <br />
-                Allowed special characters: <span>!</span> <span>@</span>{" "}
-                <span>#</span> <span>$</span> <span>%</span>
+            </section>
+          ) : (
+            <section>
+              <p ref={errRef} className={errMsg ? "errmsg" : "offscreen"}>
+                {errMsg}
               </p>
-              <label htmlFor="confirm_pwd">
-                Confirm Password:
-                <FontAwesomeIcon
-                  icon={faCheck}
-                  className={validMatch && matchPwd ? "valid" : "hide"}
+              <h1>Register</h1>
+              <form onSubmit={handleSubmit}>
+                <label htmlFor="email">
+                  Email:
+                  <FontAwesomeIcon
+                    icon={faCheck}
+                    className={validEmail ? "valid" : "hide"}
+                  />
+                  <FontAwesomeIcon
+                    icon={faTimes}
+                    className={validEmail || !email ? "hide" : "invalid"}
+                  />
+                </label>
+                <input
+                  type="text"
+                  id="email"
+                  ref={emailRef}
+                  onChange={(e) => setEmail(e.target.value)}
+                  value={email}
+                  required
+                  onFocus={() => setEmailFocus(true)}
+                  onBlur={() => setEmailFocus(false)}
                 />
-                <FontAwesomeIcon
-                  icon={faTimes}
-                  className={validMatch || !matchPwd ? "hide" : "invalid"}
+                <p
+                  id="emailnote"
+                  className={
+                    emailFocus && email && !validEmail
+                      ? "instructions"
+                      : "offscreen"
+                  }
+                >
+                  <FontAwesomeIcon icon={faInfoCircle} />
+                  Please enter a valid organisation email.
+                </p>
+                <label htmlFor="password">
+                  Password:
+                  <FontAwesomeIcon
+                    icon={faCheck}
+                    className={validPwd ? "valid" : "hide"}
+                  />
+                  <FontAwesomeIcon
+                    icon={faTimes}
+                    className={validPwd || !pwd ? "hide" : "invalid"}
+                  />
+                </label>
+                <input
+                  type="password"
+                  id="password"
+                  onChange={(e) => setPwd(e.target.value)}
+                  value={pwd}
+                  required
+                  onFocus={() => setPwdFocus(true)}
+                  onBlur={() => setPwdFocus(false)}
                 />
-              </label>
-              <input
-                type="password"
-                id="confirm_pwd"
-                onChange={(e) => setMatchPwd(e.target.value)}
-                value={matchPwd}
-                required
-                onFocus={() => setMatchFocus(true)}
-                onBlur={() => setMatchFocus(false)}
-              />
-              <p
-                id="confirmnote"
-                className={
-                  matchFocus && !validMatch ? "instructions" : "offscreen"
-                }
-              ></p>
-              <FontAwesomeIcon icon={faInfoCircle} />
-              Must match the first password input field.
-            </form>
-          </section>
+                <p
+                  id="pwdnote"
+                  className={pwdFocus && !validPwd ? "instructions" : "offscreen"}
+                >
+                  <FontAwesomeIcon icon={faInfoCircle} />
+                  8 to 24 characters.
+                  <br />
+                  Must include uppercase and lowercase letters, a number and a
+                  special character.
+                  <br />
+                  Allowed special characters: <span>!</span> <span>@</span>{" "}
+                  <span>#</span> <span>$</span> <span>%</span>
+                </p>
+                <label htmlFor="confirm_pwd">
+                  Confirm Password:
+                  <FontAwesomeIcon
+                    icon={faCheck}
+                    className={validMatch && matchPwd ? "valid" : "hide"}
+                  />
+                  <FontAwesomeIcon
+                    icon={faTimes}
+                    className={validMatch || !matchPwd ? "hide" : "invalid"}
+                  />
+                </label>
+                <input
+                  type="password"
+                  id="confirm_pwd"
+                  onChange={(e) => setMatchPwd(e.target.value)}
+                  value={matchPwd}
+                  required
+                  onFocus={() => setMatchFocus(true)}
+                  onBlur={() => setMatchFocus(false)}
+                />
+                <p
+                  id="confirmnote"
+                  className={
+                    matchFocus && !validMatch ? "instructions" : "offscreen"
+                  }
+                >
+                  <FontAwesomeIcon icon={faInfoCircle} />
+                  Must match the first password input field.
+                </p>
+                <button
+                  disabled={
+                    !validEmail || !validPwd || !validMatch ? true : false
+                  }
+                >
+                  Sign Up
+                </button>
+                <p>
+                  Already registered?
+                  <br />
+                  <span className="line">
+                    <a href="#">Sign In</a>
+                  </span>
+                </p>
+              </form>
+            </section>
+          )}
         </Grid>
       </Grid>
     </div>
