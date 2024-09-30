60f0cc6b8ab49c19c1cd7ec62bbe3443c3365d41
Sainab Omar <sainabomar07@gmail.com>
2024-09-30 17:16:07 +0100

    adds new components and updates styling

--- src/main/resources/front-end/src/App.jsx
+++ src/main/resources/front-end/src/App.jsx
@@ -1,9 +1,13 @@
 import "./App.css";
-import Register from "../src/components/Register/Register";
+import Register from "./components/Register/Register";
+import Home from "./components/Home/Home";
+import Login from "./components/Login/Login";
 function App() {
   return (
     <main>
+      <Home />
       <Register />
+      <Login />
     </main>
   );
 }
