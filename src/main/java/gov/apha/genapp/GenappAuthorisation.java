6d710fffbad6c604e4b7c418a04a5c6af101e564
Praveen Murugan <praveenmurugan@ranch-743.int.sce.network>
2024-10-17 12:22:28 +0100

    logic for basic authorisation using email and password

--- src/main/java/gov/apha/genapp/GenappAuthorisation.java
+++ src/main/java/gov/apha/genapp/GenappAuthorisation.java
@@ -1,11 +1,17 @@
 package gov.apha.genapp;
-import org.springframework.web.bind.annotation.*
+import org.springframework.web.bind.annotation.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
 
 @RestController
 @RequestMapping("/authorise")
 public class GenappAuthorisation {
+    @Autowired
+    private Argon2PasswordEncoder passwordEncoder;
+
     @PostMapping
     public String authorise(@RequestParam String email, @RequestParam String password){
-        
+        //Check for username and password encrypted with Argon2 here
+        return "sucess";
     }
 }
---
+++ src/main/java/gov/apha/genapp/UserRepository.java
@@ -0,0 +1,8 @@
+package gov.apha.genapp;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
