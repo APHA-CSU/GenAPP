0c72715090165496c7a5925e6345753a05e216f1
Praveen Murugan <praveenmurugan@ranch-743.int.sce.network>
2024-12-12 14:41:56 +0000

    Update Javadoc and copyright headers for rest controller Java classes

--- src/main/java/gov/apha/genapp/restcontroller/AuthStatus.java
+++ src/main/java/gov/apha/genapp/restcontroller/AuthStatus.java
@@ -1,3 +1,12 @@
+/** 
+ * 		(c) Crown Copyright 2024 Animal and Plant Health Agency
+ * 
+ * You may use and re-use this software and associated documentation files free of 
+ * charge in any format or medium, under the terms of the Open Government Licence v3.0.
+ * You may obtain a copy of the Open Government Licence at
+ * 
+ * 		https://www.nationalarchives.gov.uk/doc/open-government-licence/
+ */
 package gov.apha.genapp.restcontroller;
 
 import java.util.HashMap;
@@ -10,8 +19,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AuthStatus {
-    /**
-     * Endpoint to check whether the user is authenticated
+    /** 
+     * Endpoint to get authentication status object
+     * @return Map<String, Object> object with status and username
      */
     @GetMapping("/authstatus")
     public Map<String, Object> getAuthStatus() {
