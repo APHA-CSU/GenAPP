87d057747f0c4718f906f1ca1a404b157e8dcbbe
Praveen Murugan <praveenmurugan@ranch-858.int.sce.network>
2025-01-22 15:17:28 +0000

    connect our datasource and registration services with user details services for authentication

---
+++ src/main/java/gov/apha/genapp/webconfiguration/UserDetailsServiceImpl.java
@@ -0,0 +1,35 @@
+package gov.apha.genapp.webconfiguration;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import gov.apha.genapp.model.User;
+import gov.apha.genapp.model.UserRepository;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username);
+        if (user == null) {
+            throw new UsernameNotFoundException("User not available");
+        }
+        List<GrantedAuthority> authorities = new ArrayList<>();
+        authorities.add(new SimpleGrantedAuthority("ROLE_USER"));
+        return new org.springframework.security.core.userdetails.User(user.getUsername(), 
+        user.getPassword(), authorities);
+
+    }
+}
