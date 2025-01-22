ecb992ff720b261888cb037f969a579665224e2f
Praveen Murugan <praveenmurugan@ranch-858.int.sce.network>
2025-01-22 15:15:58 +0000

    defines password configuration inside Security configuration

--- src/main/java/gov/apha/genapp/webconfiguration/SecurityConfiguration.java
+++ src/main/java/gov/apha/genapp/webconfiguration/SecurityConfiguration.java
@@ -1,15 +1,20 @@
 package gov.apha.genapp.webconfiguration;
 
-import org.springframework.context.annotation.Configuration;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfiguration {
+
+        @Autowired
+        private UserDetailsServiceImpl userDetailsServiceImpl;
         /**
          * Configuration to include public endpoints and redirection to custom login
          * page
@@ -35,4 +40,10 @@ public class SecurityConfiguration {
                                                 .permitAll());
                 return http.build();
         }
+
+        @Bean
+        public PasswordEncoder passwordEncoder() {
+                return new BCryptPasswordEncoder();
+        }
+
 }
