6daec47913f1d57e77e1a2cbf5756dbaf00eed0c
Praveen Murugan <praveenmurugan@ranch-858.int.sce.network>
2025-01-22 15:12:04 +0000

    creates an User Entity that enables other services to Register or Authenticate an user

---
+++ src/main/java/gov/apha/genapp/model/User.java
@@ -0,0 +1,45 @@
+package gov.apha.genapp.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" + "id=" + id + ",username='" + username + '\'' + '}';
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+}
