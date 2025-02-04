e56a00370bb05025382c41b820190832095d6a25
Praveen Murugan <praveenmurugan@ranch-858.int.sce.network>
2025-02-04 10:49:53 +0000

    adds necessary getters and setters to user entity, and saves Verification code, expiry Time, enabled status within the user entity

--- src/main/java/gov/apha/genapp/model/User.java
+++ src/main/java/gov/apha/genapp/model/User.java
@@ -1,5 +1,7 @@
 package gov.apha.genapp.model;
 
+import java.time.LocalDateTime;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -21,12 +23,32 @@ public class User {
     @Column(nullable = false)
     private String password;
 
+    @Column(nullable=false)
+    private String verificationCode;
+
+    @Column(nullable=false)
+    private LocalDateTime expiryTime;
+
+    private Boolean enabled;
+
     public void setUsername(String username) {
         this.username = username;
     }
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public void setVerificationCode(String verificationCode) {
+        this.verificationCode = verificationCode;
+    }
+
+    public void setExpiryTime(LocalDateTime expiryTime) {
+        this.expiryTime = expiryTime;
+    }
+
+    public void setEnabled(Boolean enabled) {
+        this.enabled = enabled;
     }
 
     @Override
@@ -40,6 +62,18 @@ public class User {
 
     public String getPassword() {
         return password;
+    }
+
+    public String getVerificationCode(){
+        return verificationCode;
+    }
+
+    public LocalDateTime getExpiryTime(){
+        return expiryTime;
+    }
+
+    public Boolean getStatus(){
+        return enabled;
     }
 
 }
