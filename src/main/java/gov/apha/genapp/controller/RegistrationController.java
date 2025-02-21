ed01abab62c28690fbc4fa2a7af1b297cfa54e6d
Praveen Murugan <praveenmurugan@ranch-858.int.sce.network>
2025-02-21 13:11:08 +0000

    returns registration view for unauthorised users and returns dashboard view for authenticated users on register endpoint

--- src/main/java/gov/apha/genapp/controller/RegistrationController.java
+++ src/main/java/gov/apha/genapp/controller/RegistrationController.java
@@ -4,13 +4,19 @@ import java.time.LocalDateTime;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import gov.apha.genapp.model.User;
 import gov.apha.genapp.services.UserService;
+
 
 @Controller
 public class RegistrationController {
@@ -20,6 +26,15 @@ public class RegistrationController {
 
     @Autowired
     private PasswordEncoder passwordEncoder;
+
+    @GetMapping("/register")
+    public String showRegistrationPage(Authentication authentication) {
+        if(authentication != null) {
+            return "redirect:/index.html";
+        }
+        return "registration.html";
+    }
+    
 
     @PostMapping("/register")
     public ResponseEntity<Boolean> registerUserAccount(@RequestParam("username") String username,
