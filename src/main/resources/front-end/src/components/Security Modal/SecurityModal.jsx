c28f7e8129417478aa3fb2c6c6d21525f43760cc
Sainab Omar <sainabomar07@gmail.com>
2025-09-22 12:55:33 +0100

    creates security modal dialog

---
+++ src/main/resources/front-end/src/components/Security Modal/SecurityModal.jsx
@@ -0,0 +1,63 @@
+import "../Security Modal/SecurityModal.css";
+import { useSelector, useDispatch } from "react-redux";
+import { hideModal } from "../../features/securitySlice";
+import Dialog from "@mui/material/Dialog";
+import { useState } from "react";
+
+const SecurityModal = () => {
+  const showModal = useSelector((state) => state.security.showModal);
+  const dispatch = useDispatch();
+  const [checked, setChecked] = useState(false);
+
+  const handleCheck = (event) => {
+    setChecked(event.target.checked);
+
+    if (event.target.checked) {
+      setTimeout(() => {
+        dispatch(hideModal());
+      }, 2000);
+    }
+  };
+
+  return (
+    <Dialog
+      open={showModal}
+      slotProps={{
+        backdrop: {
+          style: { backgroundColor: "rgba(0, 0, 0, 0.8)" },
+        },
+      }}
+    >
+      <div className="dialog">
+        <h2>Important Security Message</h2>
+        <h3>Do not share login details between users</h3>
+        <h3>
+          This dashboard displays Official Sensitive data which must only be
+          viewed by those with appropriate access. Failure to comply risks
+          shutdown of the service.
+        </h3>
+        <div className="govuk-form-group">
+          <div className="govuk-checkboxes govuk-checkboxes--small">
+            <div className="govuk-checkboxes__item">
+              <input
+                className="govuk-checkboxes__input"
+                type="checkbox"
+                onChange={handleCheck}
+                disabled={checked}
+                id="checkbox--agree"
+              />
+              <label
+                className="govuk-label govuk-checkboxes__label"
+                htmlFor="checkbox--agree"
+              >
+                I agree to the above
+              </label>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Dialog>
+  );
+};
+
+export default SecurityModal;
